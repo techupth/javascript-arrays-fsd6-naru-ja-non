30e628c3e5ed1ee72aede11d65df5dae32731a98
Ja <ja.techup@gmail.com>
2024-05-13 13:54:42 +0700

    feat:js ex1-7

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,12 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+console.log(Array.isArray(orders));
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+orders[2].creditCardType = "visa";
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+orders.shift();
+
+console.log(totalPurchaseOfDary);
+console.log(orders);
